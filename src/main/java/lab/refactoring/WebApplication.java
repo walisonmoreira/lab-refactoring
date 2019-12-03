44558f6fbba65f71320027c49816c68fbbf9bd44
Walison Moreira <walison.moreira@gmail.com>
2019-12-02 21:14:56 -0300

    Comentário.

--- src/main/java/lab/refactoring/WebApplication.java
+++ src/main/java/lab/refactoring/WebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(WebApplication.class, args);
+    \\SpringApplication.run(WebApplication.class, args);
   }
 
 }
